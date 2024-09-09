15e5b0886c163b77ac6c17e892151a3081f1a26c
Antonio Whitbeck <a.whitbeck89@gmail.com>
2024-09-09 19:00:26 +0000

    started

--- src/models.py
+++ src/models.py
@@ -7,26 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "user_name": self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email
+            }
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=True)
+    orbital_period = Column(String(250), nullable=True)
+    diameter = Column(String(250), nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def serialize(self):
+        return {
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter
+            }
+    
+class Starships(Base):
+    __tablename__ = 'starships'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=True)
+    manufacturer = (String(250), nullable=True)
+    cargo_capacity = (String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+    def serialize(self):
+        return {
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cargo_capacity": self.cargo_capacity
+            }
 
-## Draw from SQLAlchemy base
+
+
 render_er(Base, 'diagram.png')
